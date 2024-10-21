864fe8c6ea153e342b0b596be20fb882e511f6d2
Yueyang Ding <dingyeuyang@gmail.com>
2024-10-21 19:29:24 -0400

    Initial Commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Yueyang Ding on 2024-10-21.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BankingTests/BankingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankingTests.swift
+//  BankingTests
+//
+//  Created by Yueyang Ding on 2024-10-21.
+//
+
+import Testing
+@testable import Banking
+
+struct BankingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
